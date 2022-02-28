720282be50954d247b278293a4f3ba66dbd6281b
Del Attipoe <delali.attipoe@hmcts.net>
2022-02-28 13:13:26 +0000

    SSCS-10152 Improve smoke test, tidy up build.gradle.

--- src/smokeTest/java/uk/gov/hmcts/reform/sscs/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/sscs/SmokeTest.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.reform.sscs;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -34,5 +37,24 @@ public class SmokeTest {
             .then()
             .assertThat()
             .statusCode(HttpStatus.OK.value());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "/health/liveness",
+        "/health/readiness"})
+    public void healthAndLivenessShouldReportUp(String healthPath) {
+
+        RequestSpecification requestSpecification = new RequestSpecBuilder()
+            .setBaseUri(testUrl)
+            .setRelaxedHTTPSValidation()
+            .build();
+
+        given(requestSpecification)
+            .when()
+            .get(healthPath)
+            .then()
+            .assertThat()
+            .statusCode(HttpStatus.OK.value())
+            .body(containsString("{\"status\":\"UP\"}"));
     }
 }
