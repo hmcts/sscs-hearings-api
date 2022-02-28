78dc6da7f3297941cf25232787fa81265263a7b4
dattipoeHMCTS <95091179+dattipoeHMCTS@users.noreply.github.com>
2022-02-28 16:19:58 +0000

    SSCS-10152 Security, dependency and general fixes. (#11)
    * SSCS-10152 Fix dependency issues, add security config, and tidy up modules.
    
    * SSCS-10152 Build up Jenkinsfiles, infrastructure files, and Gradle PACT addition.
    
    * SSCS-10152 Add demo unit test to allow Jacoco execution.
    
    * SSCS-10152 TF changes.
    
    * SSCS-10152 Improve smoke test, tidy up build.gradle.

--- src/main/java/uk/gov/hmcts/reform/sscs/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/controllers/RootController.java
@@ -1,11 +1,7 @@
 package uk.gov.hmcts.reform.sscs.controllers;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static org.springframework.http.ResponseEntity.ok;
 
 /**
  * Default endpoints per application.
@@ -13,22 +9,15 @@ import static org.springframework.http.ResponseEntity.ok;
 @RestController
 public class RootController {
 
-    @Value("${spring.application.name}")
-    private String applicationName;
-
     /**
      * Root GET endpoint.
      *
      * <p>Azure application service has a hidden feature of making requests to root endpoint when
      * "Always On" is turned on.
      * This is the endpoint to deal with that and therefore silence the unnecessary 404s as a response code.
-     *
-     * @return Welcome message from the service.
      */
     @GetMapping("/")
-    public ResponseEntity<String> welcome() {
-        String out = String.format("<div><h1>%1$s</h1><p>Welcome to %1$s</p><p>Health: <a "
-            + "href='/health'>/health</a></p><p>Swagger API Docs: <a href='/swagger-ui/index.html'>/swagger-ui/index.html</a></p></div>",applicationName);
-        return ok(out);
+    public void root() {
+        // Only used for returning a 200 on /
     }
 }
