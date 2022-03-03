3074d8457000edc42324dd5ec2910b9a24a43a98
valantinas <rokas.valantinas@hmcts.net>
2022-03-03 14:57:27 +0000

    SSCS-10218 initial commit for ref data API feign client [pair:GT]

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/domain/model/UserRequest.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.reform.sscs.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest implements Serializable {
+    private List<String> userIds;
+}
