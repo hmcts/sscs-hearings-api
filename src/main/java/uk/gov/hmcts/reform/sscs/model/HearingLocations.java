7589ebb8aa96c0c6e5d0bd9cda3c89f9e21dbccc
GurjarD <dholaram.gurjar@hmcts.net>
2022-03-04 12:24:29 +0000

    10088 POST Hearing Endpoint model mapping with validation

--- src/main/java/uk/gov/hmcts/reform/sscs/model/HearingLocations.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/HearingLocations.java
@@ -2,13 +2,20 @@ package uk.gov.hmcts.reform.sscs.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
+import uk.gov.hmcts.reform.sscs.exceptions.ValidationError;
+import uk.gov.hmcts.reform.sscs.validator.EnumPattern;
+
+import javax.validation.constraints.NotEmpty;
 
 @Builder(toBuilder = true)
-@Getter
+@Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class HearingLocations {
 
+    @NotEmpty(message = ValidationError.LOCATION_TYPE_EMPTY)
     private String locationType;
+
+    @EnumPattern(enumClass = LocationType.class, fieldName = "locationId")
     private String locationId;
 }
--- src/main/java/uk/gov/hmcts/reform/sscs/model/HearingWindow.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/HearingWindow.java
@@ -2,10 +2,11 @@ package uk.gov.hmcts.reform.sscs.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
+
 
 @Builder(toBuilder = true)
-@Getter
+@Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class HearingWindow {
 
