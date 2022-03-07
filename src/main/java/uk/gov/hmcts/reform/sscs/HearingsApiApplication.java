ed66b8887615df5c70ea0fb8aa37eaa4f73faeca
dholaramgurjar <97968145+dholaramgurjar@users.noreply.github.com>
2022-03-07 08:58:04 +0000

    SSCS-10056 Removing space from package Import

--- src/main/java/uk/gov/hmcts/reform/sscs/HearingsApiApplication.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/HearingsApiApplication.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.sscs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
