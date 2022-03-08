1907fa0c50011cdbaa606e1904424788e6109f25
Benson Li <benson.li@evoco.co.uk>
2022-03-08 22:58:56 +0000

    base on sscs 10056 update models
    base on sscs 10056 update models

--- src/main/java/uk/gov/hmcts/reform/sscs/model/PartyDetails.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/PartyDetails.java
@@ -3,20 +3,43 @@ package uk.gov.hmcts.reform.sscs.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
+import uk.gov.hmcts.reform.sscs.exceptions.ValidationError;
+import uk.gov.hmcts.reform.sscs.validator.EnumPattern;
 
 import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Builder(toBuilder = true)
-@Getter
+@Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class PartyDetails {
 
+    @NotEmpty(message = ValidationError.PARTY_DETAILS_NULL_EMPTY)
+    @Size(max = 40, message = ValidationError.PARTY_DETAILS_MAX_LENGTH)
     private String partyID;
+
+    @NotEmpty(message = ValidationError.PARTY_TYPE_EMPTY)
+    @EnumPattern(enumClass = PartyType.class, fieldName = "partyType")
     private String partyType;
+
+    @NotEmpty(message = ValidationError.PARTY_ROLE_EMPTY)
+    @Size(max = 6, message = ValidationError.PARTY_ROLE_MAX_LENGTH)
     private String partyRole;
+
+    @Valid
+    private IndividualDetails individualDetails;
+
+    @Valid
+    private OrganisationDetails organisationDetails;
+
     @JsonProperty("unavailabilityDOW")
-    private UnavailabilityDoW unavailabilityDoW;
-    private UnavailabilityRange unavailabilityRanges;
-    private List<OneOf> oneOf;
+    @Valid
+    private List<UnavailabilityDoW> unavailabilityDoW;
+
+    @Valid
+    private List<UnavailabilityRange> unavailabilityRanges;
+
 }
--- src/main/java/uk/gov/hmcts/reform/sscs/model/RequestDetails.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/RequestDetails.java
@@ -2,18 +2,20 @@ package uk.gov.hmcts.reform.sscs.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
+import uk.gov.hmcts.reform.sscs.exceptions.ValidationError;
 
+import java.time.LocalDateTime;
+import javax.validation.constraints.NotNull;
+
+@Data
 @Builder(toBuilder = true)
-@Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class RequestDetails {
 
-    private String requestTimeStamp;
-    private Number versionNumber;
+    @NotNull(message = ValidationError.REQUEST_TIMESTAMP_NULL_EMPTY)
+    private LocalDateTime requestTimeStamp;
+    @NotNull(message =  ValidationError.REQUEST_VERSION_NUMBER_NULL_EMPTY)
+    private Integer versionNumber;
 
-    private String hearingRequestID;
-    private String status;
-    private boolean hearingGroupFlag;
-    private String partiesNotified;
 }
