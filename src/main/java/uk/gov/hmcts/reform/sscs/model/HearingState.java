4abc645d4d64dbaf1db166aa7eb47114584c3f9c
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-03-10 17:10:36 +0000

    SSCS-10225 Case details to HMC Mapping

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/HearingState.java
@@ -0,0 +1,28 @@
+package uk.gov.hmcts.reform.sscs.model;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+
+@Getter
+public enum HearingState {
+    // TODO Remove this when SSCS-10222 is deployed
+
+    CREATE_HEARING("createHearing", "Create Hearing"),
+    UPDATED_CASE("updatedCase", "Case Updated"),
+    UPDATE_HEARING("updateHearing", "Update Hearing"),
+    CANCEL_HEARING("cancelHearing", "Cancel Hearing"),
+    PARTY_NOTIFIED("partyNotified", "Parties Notified");
+
+    private final String state;
+    private final String description;
+
+    HearingState(String state, String description) {
+        this.state = state;
+        this.description = description;
+    }
+
+    @JsonValue
+    public String getState() {
+        return state;
+    }
+}
