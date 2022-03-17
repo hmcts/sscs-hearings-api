463c97d99c39a4bed6ea90d3c1574a25a2cf8879
Chris Chandler <chris.chandler@hmcts.net>
2022-03-17 10:14:03 +0000

    Wiring application.yaml with @Value

--- src/main/java/uk/gov/hmcts/reform/sscs/service/DeadLetterService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/DeadLetterService.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.model.DeadLetter;
 
@@ -15,14 +15,13 @@ import uk.gov.hmcts.reform.sscs.model.DeadLetter;
 @Service
 public class DeadLetterService {
 
-    private DeadLetterService() {
-        // Gradle styleCheck
-    }
+    @Value("${azure.dead-letter-queue.connectionString}")
+    private String connectionString;
 
-    private static final String connectionString = System.getenv("AZURE_SERVICEBUS_DL_CONNECTION_STRING");
-    private static final String queueName = System.getenv("AZURE_SERVICEBUS_DL_QUEUE_NAME");
+    @Value("${azure.dead-letter-queue.queueName}")
+    private String queueName;
 
-    public static void sendMessage(DeadLetter deadLetter) throws JsonProcessingException {
+    public void sendMessage(DeadLetter deadLetter) throws JsonProcessingException {
         ServiceBusSenderClient senderClient = new ServiceBusClientBuilder()
             .connectionString(connectionString)
             .sender()
@@ -34,7 +33,7 @@ public class DeadLetterService {
         senderClient.close();
     }
 
-    private static String deadLetterToJson(DeadLetter deadLetter) throws JsonProcessingException {
+    private String deadLetterToJson(DeadLetter deadLetter) throws JsonProcessingException {
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 
         try {
