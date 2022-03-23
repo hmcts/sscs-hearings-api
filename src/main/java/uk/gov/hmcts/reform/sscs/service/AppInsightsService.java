980f1629d20e8d1325fd724cd6b31dc31b781e49
Chris Chandler <chris.chandler@hmcts.net>
2022-03-23 12:41:43 +0000

    Telemetry event to app insights

--- src/main/java/uk/gov/hmcts/reform/sscs/service/AppInsightsService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/AppInsightsService.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.reform.sscs.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.telemetry.EventTelemetry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.model.HmcFailureMessage;
@@ -11,12 +13,17 @@ import uk.gov.hmcts.reform.sscs.model.HmcFailureMessage;
 @Service
 public final class AppInsightsService {
 
+    static final TelemetryClient telemetryClient = new TelemetryClient();
+
     private AppInsightsService() {
         // Gradle style check
     }
 
-    public static void sendAppInsightsLog(HmcFailureMessage hmcFailureMessage) throws JsonProcessingException {
-        log.info(messageToJson(hmcFailureMessage));
+    public static void sendAppInsightsEvent(HmcFailureMessage hmcFailureMessage) throws JsonProcessingException {
+        String message = messageToJson(hmcFailureMessage);
+
+        telemetryClient.trackEvent(new EventTelemetry(message));
+        log.info(message);
     }
 
     private static String messageToJson(HmcFailureMessage hmcFailureMessage) throws JsonProcessingException {
