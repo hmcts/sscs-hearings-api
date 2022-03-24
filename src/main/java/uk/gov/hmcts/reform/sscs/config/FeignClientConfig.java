eff0d80bc50c757fbad8df6e3790b31b96d509f0
ThomasKKC <thomas.cheong@version1.com>
2022-03-24 15:19:37 +0000

    Initial commit on retryer and timeout configuration

--- src/main/java/uk/gov/hmcts/reform/sscs/config/FeignClientConfig.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/FeignClientConfig.java
@@ -1,10 +1,14 @@
 package uk.gov.hmcts.reform.sscs.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Retryer;
 import feign.codec.Decoder;
+import feign.codec.ErrorDecoder;
 import feign.jackson.JacksonDecoder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import uk.gov.hmcts.reform.sscs.service.exceptions.FeignClientErrorDecoder;
 
 public class FeignClientConfig {
 
@@ -13,5 +17,17 @@ public class FeignClientConfig {
     Decoder feignDecoder(ObjectMapper objectMapper) {
         return new JacksonDecoder(objectMapper);
     }
-    
+
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return new FeignClientErrorDecoder();
+    }
+
+    @Bean
+    public Retryer retryer(@Value("${feign.client.retryer.period}") long period,
+                           @Value("${feign.client.retryer.maxPeriod}") long maxPeriod,
+                           @Value("${feign.client.retryer.maxAttempts}") int maxAttempts) {
+        return new Retryer.Default(period, maxPeriod, maxAttempts);
+    }
+
 }
