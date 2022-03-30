59ce4ca7da53e884c5486e209035064bed7d68c5
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-03-30 14:02:48 +0100

    SSCS-10321 Amend Objects

--- src/main/java/uk/gov/hmcts/reform/sscs/model/HearingWrapper.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/HearingWrapper.java
@@ -1,11 +1,15 @@
 package uk.gov.hmcts.reform.sscs.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class HearingWrapper {
     private SscsCaseData originalCaseData;
