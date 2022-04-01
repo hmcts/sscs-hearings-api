897349cd118a21d80184a82768105953beee2277
TuranG <gorkem.turan@hmcts.net>
2022-04-01 14:16:59 +0100

    SSCS-10260-POJOs are updated to have builders

--- src/main/java/uk/gov/hmcts/reform/sscs/model/single/hearing/OrganisationDetails.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/single/hearing/OrganisationDetails.java
@@ -1,11 +1,16 @@
 package uk.gov.hmcts.reform.sscs.model.single.hearing;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
+
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrganisationDetails {
 
