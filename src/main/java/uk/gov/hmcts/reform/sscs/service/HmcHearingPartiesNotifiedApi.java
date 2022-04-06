14957ebc378efaaf3604b23baf4936a5a42e69ed
Chris Chandler <chris.chandler@hmcts.net>
2022-04-06 12:33:33 +0100

    Fixing wiring issue

--- src/main/java/uk/gov/hmcts/reform/sscs/service/HmcHearingPartiesNotifiedApi.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/HmcHearingPartiesNotifiedApi.java
@@ -2,15 +2,13 @@ package uk.gov.hmcts.reform.sscs.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import uk.gov.hmcts.reform.sscs.config.FeignClientConfig;
 import uk.gov.hmcts.reform.sscs.model.partiesnotified.PartiesNotified;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
+@RestController
 @FeignClient(name = "hmc-hearing", url = "${hmc.url}", configuration = FeignClientConfig.class)
 public interface HmcHearingPartiesNotifiedApi {
 
