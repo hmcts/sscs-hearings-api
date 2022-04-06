58940a89f2080684cd5530ff394d0fbf5b6827b7
Chris Chandler <chris.chandler@hmcts.net>
2022-04-06 13:18:43 +0100

    Config for queue retry options

--- src/main/java/uk/gov/hmcts/reform/sscs/config/HearingsEventQueueListener.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/HearingsEventQueueListener.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.sscs.config;
 
+import com.azure.core.amqp.AmqpRetryOptions;
 import com.azure.messaging.servicebus.*;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.sscs.helper.QueueHelper;
 import uk.gov.hmcts.reform.sscs.ccd.domain.HearingRequest;
 
+import java.time.Duration;
 import java.util.concurrent.CountDownLatch;
 
 @Slf4j
@@ -19,11 +21,18 @@ public class HearingsEventQueueListener {
     private String connectionString;
     @Value("${azure.hearings-queue.topicName}")
     private String topicName;
+    @Value("${azure.hearings-queue.timeout}")
+    private Duration tryTimeout;
+    @Value("${azure.hearings-queue.delay}")
+    private Duration delay;
+    @Value("${azure.hearings-queue.retries}")
+    private Integer maxRetries;
 
     public void receiveMessages() {
         CountDownLatch countdownLatch = new CountDownLatch(1);
 
         ServiceBusProcessorClient processorClient = new ServiceBusClientBuilder()
+            .retryOptions(configureRetryOptions())
             .connectionString(connectionString)
             .processor()
             .topicName(topicName)
@@ -38,5 +47,18 @@ public class HearingsEventQueueListener {
         ServiceBusReceivedMessage message = context.getMessage();
         HearingRequest hearingRequest = message.getBody().toObject(HearingRequest.class);
         log.info("Message {} received from Hearings Event Queue for Case ID {}", hearingRequest.toString(), hearingRequest.getCcdCaseId());
+    }
+
+    private AmqpRetryOptions configureRetryOptions() {
+        AmqpRetryOptions amqpRetryOptions = new AmqpRetryOptions();
+
+        if (tryTimeout != null)
+            amqpRetryOptions.setTryTimeout(tryTimeout);
+        if (maxRetries != null)
+            amqpRetryOptions.setMaxRetries(maxRetries);
+        if (delay != null)
+            amqpRetryOptions.setDelay(delay);
+
+        return amqpRetryOptions;
     }
 }
