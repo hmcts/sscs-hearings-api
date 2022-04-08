202a1f4b1b431a4d33dfaa3af4458687667605ba
suetlaiHMCTS <suetlai.chan@hmcts.net>
2022-04-08 12:04:50 +0100

    SSCS-10360 remove unused library

--- src/main/java/uk/gov/hmcts/reform/sscs/model/single/hearing/HearingResponse.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/single/hearing/HearingResponse.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.sscs.model.single.hearing;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.sscs.validator.EnumPattern;
