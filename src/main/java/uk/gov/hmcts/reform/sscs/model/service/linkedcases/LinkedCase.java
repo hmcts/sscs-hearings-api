509901d3d0e0fb2851f2d35c79feefa2cc48a236
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-05-04 13:03:06 +0100

    SSCS-10262 ServiceLinkedCases Endpoint

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/service/linkedcases/LinkedCase.java
@@ -0,0 +1,16 @@
+package uk.gov.hmcts.reform.sscs.model.service.linkedcases;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class LinkedCase {
+    private String ccdCaseId;
+}
