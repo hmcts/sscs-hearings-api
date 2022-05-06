3cc1af2626357a9e2f119e622aa3487c278d886b
suetlaiHMCTS <suetlai.chan@hmcts.net>
2022-05-06 17:03:51 +0100

    sscs-10245 remove unuse library for style checking

--- src/test/java/uk/gov/hmcts/reform/sscs/validator/EnumPatternValidatorTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/validator/EnumPatternValidatorTest.java
@@ -2,21 +2,15 @@ package uk.gov.hmcts.reform.sscs.validator;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 import uk.gov.hmcts.reform.sscs.model.single.hearing.HearingLocations;
 import uk.gov.hmcts.reform.sscs.model.single.hearing.LocationType;
 
 import java.util.Set;
-import java.util.stream.Collectors;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EnumPatternValidatorTest {
