3137c489d955d547be57f8908ab1ff88645b3e93
dattipoeHMCTS <95091179+dattipoeHMCTS@users.noreply.github.com>
2022-05-12 14:25:05 +0100

    Sscs 10301 hearings integration fixes (#132)
    * SSCS-10301 Change Servicebus receiver implementation to allow message processing after error.
    
    * SSCS-10301 Make listener run asynchronously, use FilterChain to secure ServiceHearingValuesController.
    
    * SSCS-10301 Recover original failure message in FeignClientErrorDecoder.
    
    * SSCS-10301 Improve exception handling in TribunalsHearingsEventQueueListenerConfig, tidy up ServiceHearingsController and FeignClientErrorDecoder.

--- src/integrationTest/java/uk/gov/hmcts/reform/sscs/controllers/ServiceHearingsControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/sscs/controllers/ServiceHearingsControllerTest.java
@@ -17,30 +17,22 @@ import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.sscs.ccd.domain.*;
 import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
-import uk.gov.hmcts.reform.sscs.exception.AuthorisationException;
-import uk.gov.hmcts.reform.sscs.exception.InvalidHeaderException;
 import uk.gov.hmcts.reform.sscs.idam.IdamService;
 import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
 import uk.gov.hmcts.reform.sscs.model.service.hearingvalues.ServiceHearingValues;
 import uk.gov.hmcts.reform.sscs.model.service.linkedcases.LinkedCase;
 import uk.gov.hmcts.reform.sscs.model.service.linkedcases.ServiceLinkedCases;
-import uk.gov.hmcts.reform.sscs.service.AuthorisationService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.matches;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.willDoNothing;
-import static org.mockito.BDDMockito.willThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static uk.gov.hmcts.reform.sscs.service.AuthorisationService.SERVICE_AUTHORISATION_HEADER;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -52,11 +44,6 @@ class ServiceHearingsControllerTest {
     private static final long CASE_ID_LINKED = 3456385374124L;
     private static final long MISSING_CASE_ID = 99250807409918L;
     private static final String BAD_CASE_ID = "ABCASDEF";
-    private static final String AUTHORIZATION = "Bearer eyJhbGciOiJIUzUxMiL7.eyJzdWIiOiJzc2NzIiwiZXhwIjoxNjQ2NDA5MjM5fQ"
-            + ".zSEbvMJedOGo16yBOXecLgucWyavnoVu023cterreUF0sxPlmV-Qu8Y7OloJUKrLGlNweUr8mVpYWPzE0iNyYw";
-    private static final String BAD_AUTHORIZATION = "eyJhbGciOiJIUzUxMiL7.eyJzdWIiOiJzc2NzIiwiZXhwIjoxNjQ2NDA5MjM5fQ"
-            + ".zSEbvMJedOGo16yBOXecLgucWyavnoVu023cterreUF0sxPlmV-Qu8Y7OloJUKrLGlNweUr8mVpYWPzE0iNyYw";
-    private static final String NOT_AUTHORIZATION = "Bearer notauthed";
     private static final long HEARING_ID = 123L;
     private static final String CASE_REFERENCE = "caseReference";
     private static final String CASE_REFERENCE_PARAM = "hearingId";
@@ -73,8 +60,6 @@ class ServiceHearingsControllerTest {
     @MockBean
     private IdamService idamApiService;
 
-    @MockBean
-    private AuthorisationService authorisationService;
 
     @MockBean
     private AuthTokenGenerator authTokenGenerator;
@@ -83,7 +68,7 @@ class ServiceHearingsControllerTest {
     private CcdService ccdService;
 
     @BeforeEach
-    void setUp() throws AuthorisationException, InvalidHeaderException {
+    void setUp()  {
         List<CaseLink> linkedCases = new ArrayList<>();
         linkedCases.add(CaseLink.builder()
                 .value(CaseLinkDetails.builder()
@@ -101,11 +86,6 @@ class ServiceHearingsControllerTest {
         given(ccdService.getByCaseId(eq(CASE_ID), any(IdamTokens.class))).willReturn(caseDetails);
         given(authTokenGenerator.generate()).willReturn("s2s token");
         given(idamApiService.getIdamTokens()).willReturn(IdamTokens.builder().build());
-        willThrow(new InvalidHeaderException(new Exception("Test")))
-                .given(authorisationService).authorise(anyString());
-        willThrow(new AuthorisationException(new Exception("Test")))
-                .given(authorisationService).authorise(matches("Bearer .+"));
-        willDoNothing().given(authorisationService).authorise(AUTHORIZATION);
     }
 
     // TODO These are holder tests that will need to be implemented alongside service hearing controller
@@ -118,7 +98,6 @@ class ServiceHearingsControllerTest {
         String json = asJsonString(model);
 
         mockMvc.perform(post(SERVICE_HEARING_VALUES_URL)
-                    .header(SERVICE_AUTHORISATION_HEADER, AUTHORIZATION)
                     .param(CASE_REFERENCE, String.valueOf(CASE_ID)))
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -130,32 +109,9 @@ class ServiceHearingsControllerTest {
     void testPostRequestServiceHearingValues_badCaseID() throws Exception {
 
         mockMvc.perform(post(SERVICE_HEARING_VALUES_URL)
-                        .header(SERVICE_AUTHORISATION_HEADER, AUTHORIZATION)
                         .param(CASE_REFERENCE,BAD_CASE_ID))
                 .andDo(print())
                 .andExpect(status().isBadRequest());
-    }
-
-    @DisplayName("When Authorization is incorrectly formatted should return a with 400 response code")
-    @Test
-    void testPostRequestServiceHearingValues_badAuthHeader() throws Exception {
-
-        mockMvc.perform(post(SERVICE_HEARING_VALUES_URL)
-                        .header(SERVICE_AUTHORISATION_HEADER, BAD_AUTHORIZATION)
-                        .param(CASE_REFERENCE, String.valueOf(CASE_ID)))
-                .andDo(print())
-                .andExpect(status().isBadRequest());
-    }
-
-    @DisplayName("When not Authorized should return a with 403 response code")
-    @Test
-    void testPostRequestServiceHearingValues_unauthorised() throws Exception {
-
-        mockMvc.perform(post(SERVICE_HEARING_VALUES_URL)
-                        .header(SERVICE_AUTHORISATION_HEADER, NOT_AUTHORIZATION)
-                        .param(CASE_REFERENCE, String.valueOf(CASE_ID)))
-                .andDo(print())
-                .andExpect(status().isForbidden());
     }
 
     @DisplayName("When Case Not Found should return a with 404 response code")
@@ -163,7 +119,6 @@ class ServiceHearingsControllerTest {
     void testPostRequestServiceHearingValues_missingCase() throws Exception {
 
         mockMvc.perform(post(SERVICE_HEARING_VALUES_URL)
-                        .header(SERVICE_AUTHORISATION_HEADER, AUTHORIZATION)
                         .param(CASE_REFERENCE, String.valueOf(MISSING_CASE_ID)))
                 .andDo(print())
                 .andExpect(status().isNotFound());
@@ -178,7 +133,6 @@ class ServiceHearingsControllerTest {
         String json = asJsonString(model);
 
         mockMvc.perform(post(SERVICE_LINKED_CASES_URL)
-                        .header(SERVICE_AUTHORISATION_HEADER, AUTHORIZATION)
                         .param(CASE_REFERENCE, String.valueOf(CASE_ID))
                         .param(CASE_REFERENCE_PARAM, String.valueOf(HEARING_ID)))
                 .andDo(print())
@@ -191,43 +145,18 @@ class ServiceHearingsControllerTest {
     void testPostRequestServiceLinkedCases_badCaseID() throws Exception {
 
         mockMvc.perform(post(SERVICE_LINKED_CASES_URL)
-                        .header(SERVICE_AUTHORISATION_HEADER, AUTHORIZATION)
                         .param(CASE_REFERENCE,BAD_CASE_ID)
                         .param(CASE_REFERENCE_PARAM, String.valueOf(HEARING_ID)))
                 .andDo(print())
                 .andExpect(status().isBadRequest());
     }
 
-    @DisplayName("When Authorization is incorrectly formatted should return a with 400 response code")
-    @Test
-    void testPostRequestServiceLinkedCases_badAuthHeader() throws Exception {
-
-        mockMvc.perform(post(SERVICE_LINKED_CASES_URL)
-                        .header(SERVICE_AUTHORISATION_HEADER, BAD_AUTHORIZATION)
-                        .param(CASE_REFERENCE, String.valueOf(CASE_ID))
-                        .param(CASE_REFERENCE_PARAM, String.valueOf(HEARING_ID)))
-                .andDo(print())
-                .andExpect(status().isBadRequest());
-    }
-
-    @DisplayName("When not Authorized should return a with 403 response code")
-    @Test
-    void testPostRequestServiceLinkedCases_unauthorised() throws Exception {
-
-        mockMvc.perform(post(SERVICE_LINKED_CASES_URL)
-                        .header(SERVICE_AUTHORISATION_HEADER, NOT_AUTHORIZATION)
-                        .param(CASE_REFERENCE, String.valueOf(CASE_ID))
-                        .param(CASE_REFERENCE_PARAM, String.valueOf(HEARING_ID)))
-                .andDo(print())
-                .andExpect(status().isForbidden());
-    }
 
     @DisplayName("When Case Not Found should return a with 404 response code")
     @Test
     void testPostRequestServiceLinkedCases_missingCase() throws Exception {
 
         mockMvc.perform(post(SERVICE_LINKED_CASES_URL)
-                        .header(SERVICE_AUTHORISATION_HEADER, AUTHORIZATION)
                         .param(CASE_REFERENCE, String.valueOf(MISSING_CASE_ID))
                         .param(CASE_REFERENCE_PARAM, String.valueOf(HEARING_ID)))
                 .andDo(print())
