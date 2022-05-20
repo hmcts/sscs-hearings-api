fec3ededc1b4a1efb54948411003d8133322e60e
omer-hmcts <omer.abdullah@HMCTS.net>
2022-05-20 11:14:09 +0100

    SSCS-10138 Removed unnecessary PMD suppression

--- src/main/java/uk/gov/hmcts/reform/sscs/helper/mapping/HearingsPartiesMapping.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/helper/mapping/HearingsPartiesMapping.java
@@ -27,7 +27,7 @@ import static uk.gov.hmcts.reform.sscs.reference.data.mappings.HearingChannel.NO
 import static uk.gov.hmcts.reform.sscs.reference.data.mappings.HearingChannel.TELEPHONE;
 import static uk.gov.hmcts.reform.sscs.reference.data.mappings.HearingChannel.VIDEO;
 
-@SuppressWarnings({"PMD.UnnecessaryLocalBeforeReturn","PMD.ReturnEmptyCollectionRatherThanNull", "PMD.GodClass", "PMD.NullAssignment"})
+@SuppressWarnings({"PMD.UnnecessaryLocalBeforeReturn","PMD.ReturnEmptyCollectionRatherThanNull", "PMD.GodClass"})
 // TODO Unsuppress in future
 public final class HearingsPartiesMapping {
 
