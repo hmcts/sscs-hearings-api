9701e56fa5875932a17c25aeabdc01720011dbf4
Benson Li <benson.li@evoco.co.uk>
2022-05-23 12:56:17 +0100

    update for common updated

--- src/test/java/uk/gov/hmcts/reform/sscs/helper/mapping/HearingsMappingTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/helper/mapping/HearingsMappingTest.java
@@ -23,34 +23,34 @@ class HearingsMappingTest extends HearingsMappingBase {
     @Test
     void buildHearingPayload() {
         SscsCaseData caseData = SscsCaseData.builder()
-            .ccdCaseId(String.valueOf(CASE_ID))
-            .benefitCode(BENEFIT_CODE)
-            .issueCode(ISSUE_CODE)
-            .caseCreated(CASE_CREATED)
-            .caseAccessManagementFields(CaseAccessManagementFields.builder()
-                                            .caseNameHmctsInternal(CASE_NAME_INTERNAL)
-                                            .caseNamePublic(CASE_NAME_PUBLIC)
-                                            .build())
-            .appeal(Appeal.builder()
+                .ccdCaseId(String.valueOf(CASE_ID))
+                .benefitCode(BENEFIT_CODE)
+                .issueCode(ISSUE_CODE)
+                .caseCreated(CASE_CREATED)
+                .caseAccessManagementFields(CaseAccessManagementFields.builder()
+                        .caseNameHmctsInternal(CASE_NAME_INTERNAL)
+                        .caseNamePublic(CASE_NAME_PUBLIC)
+                        .build())
+                .appeal(Appeal.builder()
                         .hearingOptions(HearingOptions.builder().build())
                         .appellant(Appellant.builder()
-                                       .id("1")
-                                       .name(Name.builder()
-                                                 .title("title")
-                                                 .firstName("first")
-                                                 .lastName("last")
-                                                 .build())
-                                       .build())
-                        .build())
-            .caseManagementLocation(CaseManagementLocation.builder()
-                                        .baseLocation(EPIMS_ID)
-                                        .region(REGION)
+                                .id("1")
+                                .name(Name.builder()
+                                        .title("title")
+                                        .firstName("first")
+                                        .lastName("last")
                                         .build())
-            .build();
+                                .build())
+                        .build())
+                .caseManagementLocation(CaseManagementLocation.builder()
+                        .baseLocation(EPIMS_ID)
+                        .region(REGION)
+                        .build())
+                .build();
         HearingWrapper wrapper = HearingWrapper.builder()
-            .caseData(caseData)
-            .caseData(caseData)
-            .build();
+                .caseData(caseData)
+                .caseData(caseData)
+                .build();
         HearingRequestPayload result = HearingsMapping.buildHearingPayload(wrapper);
 
         assertThat(result).isNotNull();
@@ -65,22 +65,22 @@ class HearingsMappingTest extends HearingsMappingBase {
     void updateIds() {
         List<CcdValue<OtherParty>> otherParties = new ArrayList<>();
         otherParties.add(new CcdValue<>(OtherParty.builder()
-                                            .id("2")
-                                            .appointee(Appointee.builder().build())
-                                            .rep(Representative.builder().build())
-                                            .build()));
+                .id("2")
+                .appointee(Appointee.builder().build())
+                .rep(Representative.builder().build())
+                .build()));
         otherParties.add(new CcdValue<>(OtherParty.builder().build()));
         SscsCaseData caseData = SscsCaseData.builder()
-            .appeal(Appeal.builder()
+                .appeal(Appeal.builder()
                         .appellant(Appellant.builder().appointee(Appointee.builder().build()).build())
                         .rep(Representative.builder().build())
                         .build())
-            .otherParties(otherParties)
-            .build();
+                .otherParties(otherParties)
+                .build();
         HearingWrapper wrapper = HearingWrapper.builder()
-            .caseData(caseData)
-            .caseData(caseData)
-            .build();
+                .caseData(caseData)
+                .caseData(caseData)
+                .build();
 
         HearingsMapping.updateIds(wrapper);
 
@@ -117,7 +117,7 @@ class HearingsMappingTest extends HearingsMappingBase {
         otherParties.add(new CcdValue<>(OtherParty.builder().id("2").build()));
 
         List<Integer> result = HearingsMapping.getAllIds(otherParties, appellant, rep);
-        List<Integer> expected = List.of(1, 2, 3, 6);
+        List<Integer> expected = List.of(1,2,3,6);
 
         assertEquals(expected, result.stream().sorted().collect(Collectors.toList()));
     }
@@ -129,7 +129,7 @@ class HearingsMappingTest extends HearingsMappingBase {
         Representative rep = Representative.builder().id("3").build();
 
         List<Integer> result = HearingsMapping.getAllPartyIds(party, rep);
-        List<Integer> expected = List.of(1, 2, 3);
+        List<Integer> expected = List.of(1,2,3);
 
         assertEquals(expected, result.stream().sorted().collect(Collectors.toList()));
     }
