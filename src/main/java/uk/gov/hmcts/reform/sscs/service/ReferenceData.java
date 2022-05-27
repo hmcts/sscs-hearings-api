9f7562c5e861ac50046b43b6de16a2434a9a3d9b
dattipoeHMCTS <95091179+dattipoeHMCTS@users.noreply.github.com>
2022-05-27 15:17:17 +0100

    SSCS-10260 Fix ReferenceData wiring

--- src/main/java/uk/gov/hmcts/reform/sscs/service/ReferenceData.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/ReferenceData.java
@@ -1,20 +1,22 @@
 package uk.gov.hmcts.reform.sscs.service;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Component
 public class ReferenceData {
+    @Autowired
     private HearingDurationsService hearingDurations;
+    
+    @Autowired
     private SessionCategoryMapService sessionCategoryMaps;
+    
     @Value("${exui.url}")
     private String exUiUrl;
+    
     @Value("${sscs.serviceCode}")
     private String sscsServiceCode;
 
