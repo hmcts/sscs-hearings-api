8d8097922cb70e9f81f1582961309b1ca04ba49d
Del Attipoe <delali.attipoe@hmcts.net>
2022-05-29 12:06:23 +0100

    SSCS-10579 Switch to session transactions.

--- src/main/java/uk/gov/hmcts/reform/sscs/config/HmcHearingsEventTopicListener.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/HmcHearingsEventTopicListener.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.qpid.jms.message.JmsBytesMessage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,7 @@ import uk.gov.hmcts.reform.sscs.exception.GetCaseException;
 import uk.gov.hmcts.reform.sscs.exception.UpdateCaseException;
 import uk.gov.hmcts.reform.sscs.model.hmcmessage.HmcMessage;
 
+import javax.jms.JMSException;
 import java.nio.charset.StandardCharsets;
 
 @Slf4j
@@ -23,9 +25,6 @@ import java.nio.charset.StandardCharsets;
 public class HmcHearingsEventTopicListener {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    @Value("${azure.service-bus.hmc-to-hearings-api.maxRetries}")
-    private Integer maxRetries;
-
     @Value("${sscs.serviceCode}")
     private String serviceCode;
 
@@ -34,37 +33,34 @@ public class HmcHearingsEventTopicListener {
         subscription = "${azure.service-bus.hmc-to-hearings-api.subscriptionName}",
         containerFactory = "hmcHearingsEventTopicContainerFactory"
     )
-    public void onMessage(byte[] messageBytes) throws JsonProcessingException, UpdateCaseException, GetCaseException {
-        String message = new String(messageBytes, StandardCharsets.UTF_8);
-        HmcMessage hmcMessage = OBJECT_MAPPER.readValue(message, HmcMessage.class);
+    public void onMessage(JmsBytesMessage message) throws JMSException {
 
-        if (isMessageRelevantForService(hmcMessage, serviceCode)) {
-            log.info("Processing hearing ID: {} for case reference: {}", hmcMessage.getHearingID(),
-                hmcMessage.getCaseRef());
-            processMessageWithRetry(hmcMessage, 1);
-        }
-    }
-
-    private void processMessageWithRetry(HmcMessage hmcMessage, int retry) throws UpdateCaseException, GetCaseException {
+        byte[] messageBytes = new byte[(int) message.getBodyLength()];
+        message.readBytes(messageBytes);
+        String convertedMessage = new String(messageBytes, StandardCharsets.UTF_8);
 
         try {
-            processMessage(hmcMessage);
-        } catch (GetCaseException | UpdateCaseException e) {
-            if (retry > maxRetries) {
-                log.error("Maximum retries exceeded for case reference: {} ", hmcMessage.getCaseRef());
-                throw e;
-            } else {
-                processMessageWithRetry(hmcMessage, ++retry);
+            HmcMessage hmcMessage = OBJECT_MAPPER.readValue(convertedMessage, HmcMessage.class);
+
+            if (isMessageRelevantForService(hmcMessage, serviceCode)) {
+                log.info("Processing hearing ID: {} for case reference: {}", hmcMessage.getHearingID(),
+                    hmcMessage.getCaseRef());
+
+                processMessage(hmcMessage);
             }
+        }  catch (GetCaseException | UpdateCaseException | JsonProcessingException ex) {
+            log.error("Unable to successfully deliver HMC message: {}", convertedMessage);
+            throw new JMSException(ex.getMessage());
         }
     }
+
 
     private void processMessage(HmcMessage message) throws GetCaseException, UpdateCaseException{
         //process.
     }
 
-    private boolean isMessageRelevantForService(HmcMessage hmcMessage, String serviceId) {
-        return hmcMessage.getHmctsServiceCode().contains(serviceId);
+    private boolean isMessageRelevantForService(HmcMessage hmcMessage, String serviceCode) {
+        return serviceCode.equals(hmcMessage.getHmctsServiceCode());
     }
 
 }
