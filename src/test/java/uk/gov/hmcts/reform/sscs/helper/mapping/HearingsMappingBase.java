a5077a75353d01fdfb847fc2391ea157f295dcf0
Bhabani Samantaray <bhabani.samantaray@hmcts.net>
2022-06-06 17:54:40 +0100

    refactor: use correct ReferenceDataServiceHolder

--- src/test/java/uk/gov/hmcts/reform/sscs/helper/mapping/HearingsMappingBase.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/helper/mapping/HearingsMappingBase.java
@@ -8,7 +8,7 @@ import uk.gov.hmcts.reform.sscs.reference.data.service.HearingDurationsService;
 import uk.gov.hmcts.reform.sscs.reference.data.service.SessionCategoryMapService;
 import uk.gov.hmcts.reform.sscs.reference.data.service.SignLanguagesService;
 import uk.gov.hmcts.reform.sscs.reference.data.service.VerbalLanguagesService;
-import uk.gov.hmcts.reform.sscs.service.ReferenceDataServiceHolder;
+import uk.gov.hmcts.reform.sscs.service.holder.ReferenceDataServiceHolder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
