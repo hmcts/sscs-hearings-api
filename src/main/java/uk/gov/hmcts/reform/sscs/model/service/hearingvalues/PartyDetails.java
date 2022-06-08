8d5f159fa37820fde527c666111cda8178913643
Del Attipoe <delali.attipoe@hmcts.net>
2022-06-08 16:46:18 +0100

    SSCS-10636 Make SHV changes.

--- src/main/java/uk/gov/hmcts/reform/sscs/model/service/hearingvalues/PartyDetails.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/service/hearingvalues/PartyDetails.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.sscs.model.service.hearingvalues;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,7 +16,6 @@ import java.util.List;
 
 @Data
 @Builder(toBuilder = true)
-@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
 @NoArgsConstructor
