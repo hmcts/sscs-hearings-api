753a412e6bcb65a1b663c551928bd2c22d8c5b4d
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2022-06-09 16:35:18 +0100

    SSCS-10493 Fix issues

--- src/main/java/uk/gov/hmcts/reform/sscs/jms/listener/TribunalsHearingsEventQueueListener.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/jms/listener/TribunalsHearingsEventQueueListener.java
@@ -1,21 +1,16 @@
 package uk.gov.hmcts.reform.sscs.jms.listener;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.sscs.ccd.domain.HearingState;
 import uk.gov.hmcts.reform.sscs.exception.GetCaseException;
-import uk.gov.hmcts.reform.sscs.exception.InvalidIdException;
 import uk.gov.hmcts.reform.sscs.exception.InvalidMappingException;
 import uk.gov.hmcts.reform.sscs.exception.TribunalsEventProcessingException;
 import uk.gov.hmcts.reform.sscs.exception.UnhandleableHearingStateException;
 import uk.gov.hmcts.reform.sscs.exception.UpdateCaseException;
-import uk.gov.hmcts.reform.sscs.model.TribunalsDeadLetterMessage;
 import uk.gov.hmcts.reform.sscs.model.hearings.HearingRequest;
-import uk.gov.hmcts.reform.sscs.service.AppInsightsService;
 import uk.gov.hmcts.reform.sscs.service.HearingsService;
 
 @Slf4j
@@ -23,13 +18,10 @@ import uk.gov.hmcts.reform.sscs.service.HearingsService;
 @ConditionalOnProperty("flags.tribunals-to-hearings-api.enabled")
 public class TribunalsHearingsEventQueueListener {
 
-    private final ObjectMapper objectMapper;
-
     private final HearingsService hearingsService;
 
     public TribunalsHearingsEventQueueListener(HearingsService hearingsService) {
         this.hearingsService = hearingsService;
-        this.objectMapper = new ObjectMapper();
     }
 
     @JmsListener(
@@ -48,9 +40,8 @@ public class TribunalsHearingsEventQueueListener {
 
             hearingsService.processHearingRequest(message);
             log.info("Hearing event {} for case ID {} successfully processed", event, caseId);
-        } catch (GetCaseException | UnhandleableHearingStateException | UpdateCaseException | InvalidIdException
+        } catch (GetCaseException | UnhandleableHearingStateException | UpdateCaseException
                  | InvalidMappingException ex) {
-            ex.printStackTrace();
             log.error("An exception occurred whilst processing hearing event for case ID {}."
                           + " Abandoning message", caseId, ex);
             throw new TribunalsEventProcessingException("Abandoned Message", ex);
