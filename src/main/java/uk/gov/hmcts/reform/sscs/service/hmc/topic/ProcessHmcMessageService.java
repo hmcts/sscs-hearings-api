88c1a65db9b0db295a4b682d1e19775ed682a792
Del Attipoe <delali.attipoe@hmcts.net>
2022-06-10 11:42:40 +0100

    SSCS-10286 State -> Event changes.

--- src/main/java/uk/gov/hmcts/reform/sscs/service/hmc/topic/ProcessHmcMessageService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/hmc/topic/ProcessHmcMessageService.java
@@ -1,8 +1,14 @@
 package uk.gov.hmcts.reform.sscs.service.hmc.topic;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.exception.CaseException;
 import uk.gov.hmcts.reform.sscs.exception.InvalidHmcMessageException;
@@ -10,10 +16,13 @@ import uk.gov.hmcts.reform.sscs.exception.MessageProcessingException;
 import uk.gov.hmcts.reform.sscs.model.hmc.message.HmcMessage;
 import uk.gov.hmcts.reform.sscs.model.hmc.reference.HmcStatus;
 import uk.gov.hmcts.reform.sscs.model.single.hearing.HearingGetResponse;
+import uk.gov.hmcts.reform.sscs.model.single.hearing.HearingResponse;
+import uk.gov.hmcts.reform.sscs.model.single.hearing.RequestDetails;
 import uk.gov.hmcts.reform.sscs.service.CcdCaseService;
 import uk.gov.hmcts.reform.sscs.service.HmcHearingApiService;
 
 import static java.util.Objects.nonNull;
+import static uk.gov.hmcts.reform.sscs.model.hmc.reference.HmcStatus.AWAITING_LISTING;
 import static uk.gov.hmcts.reform.sscs.model.hmc.reference.HmcStatus.CANCELLED;
 import static uk.gov.hmcts.reform.sscs.model.hmc.reference.HmcStatus.EXCEPTION;
 import static uk.gov.hmcts.reform.sscs.model.hmc.reference.HmcStatus.LISTED;
@@ -26,47 +35,39 @@ import static uk.gov.hmcts.reform.sscs.model.hmc.reference.ListingStatus.FIXED;
 public class ProcessHmcMessageService {
 
     private final HmcHearingApiService hmcHearingApiService;
+
     private final CcdCaseService ccdCaseService;
-    private final CaseStateUpdateService caseStateUpdateService;
+
+    private final HearingUpdateService hearingUpdateService;
 
     public void processEventMessage(HmcMessage hmcMessage)
             throws CaseException, MessageProcessingException {
 
         Long caseId = hmcMessage.getCaseId();
         String hearingId = hmcMessage.getHearingId();
+
         HmcStatus hmcMessageStatus = hmcMessage.getHearingUpdate().getHmcStatus();
 
-        log.info("Attempting to process message from hearings topic, processing for hearing status '{}', Case ID {}, Hearing ID {} and Service Code {}",
-                hmcMessageStatus.getLabel(),
-                caseId,
-                hearingId,
-                hmcMessage.getHmctsServiceCode());
-
         HearingGetResponse hearingResponse = hmcHearingApiService.getHearingRequest(hearingId);
+
         HmcStatus hmcStatus = hearingResponse.getRequestDetails().getStatus();
 
-        if (hmcMessageStatus != hmcStatus) {
-            throw new InvalidHmcMessageException(String.format("HMC Message Status '%s' does not match the GET request status '%s' "
-                    + "for Case ID %s and Hearing ID %s",
-                    hmcMessageStatus, hmcStatus, caseId, hearingId));
-        }
+        checkStatuses(caseId, hearingId, hmcMessageStatus, hmcStatus);
 
         SscsCaseData caseData = ccdCaseService.getCaseDetails(caseId).getData();
 
-        if (isHearingUpdated(hmcStatus, hearingResponse)) {
-            caseStateUpdateService.updateListed(hearingResponse, caseData);
-        } else if (isHearingCancelled(hmcStatus, hearingResponse)) {
-            caseStateUpdateService.updateCancelled(hearingResponse, caseData);
-        } else if (isStatusException(hmcStatus)) {
-            caseStateUpdateService.updateFailed(caseData);
-        } else {
+        if (stateNotHandled(hmcStatus, hearingResponse)) {
             log.info("CCD state has not been updated for the Hearing ID {} and Case ID {}",
-                    hearingId,
-                    caseId);
+                hearingId, caseId);
             return;
         }
 
+        if (isHearingUpdated(hmcStatus, hearingResponse)) {
+            hearingUpdateService.updateHearing(hearingResponse, caseData);
+        }
+
         String ccdUpdateDescription = String.format(hmcStatus.getCcdUpdateDescription(), hearingId);
+
         ccdCaseService.updateCaseData(caseData,
                 hmcStatus.getCcdUpdateEventType(),
                 hmcStatus.getCcdUpdateSummary(),
@@ -77,13 +78,28 @@ public class ProcessHmcMessageService {
                 hmcMessage.getCaseId());
     }
 
+    private void checkStatuses(Long caseId, String hearingId, HmcStatus hmcMessageStatus, HmcStatus hmcStatus)
+        throws InvalidHmcMessageException {
+        if (hmcMessageStatus != hmcStatus) {
+            throw new InvalidHmcMessageException(String.format("HMC Message Status '%s' does not match the GET request status '%s' "
+                    + "for Case ID %s and Hearing ID %s",
+                hmcMessageStatus, hmcStatus, caseId, hearingId));
+        }
+    }
+
+
+    private boolean stateNotHandled(HmcStatus hmcStatus, HearingGetResponse hearingResponse) {
+        return !(isHearingUpdated(hmcStatus, hearingResponse) ||isHearingCancelled(hmcStatus, hearingResponse)
+            || isStatusException(hmcStatus));
+    }
+
     private boolean isHearingUpdated(HmcStatus hmcStatus, HearingGetResponse hearingResponse) {
         return isHearingListedOrUpdateSubmitted(hmcStatus)
                 && isStatusFixed(hearingResponse);
     }
 
     private boolean isHearingListedOrUpdateSubmitted(HmcStatus hmcStatus) {
-        return hmcStatus == LISTED || hmcStatus == UPDATE_SUBMITTED;
+        return hmcStatus == LISTED || hmcStatus == AWAITING_LISTING || hmcStatus == UPDATE_SUBMITTED;
     }
 
     private boolean isStatusFixed(HearingGetResponse hearingResponse) {
