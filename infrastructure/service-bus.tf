0f14614d8d737cb081b9b0dfba0faae218cd8337
amoslv1 <110455092+amoslv1@users.noreply.github.com>
2022-09-01 14:31:01 +0100

    SSCS-10829 adds BBA3 subscription filter
    branched off Demo

--- infrastructure/service-bus.tf
+++ infrastructure/service-bus.tf
@@ -22,3 +22,14 @@ resource "azurerm_key_vault_secret" "hmc-to-sscs-hearings-api-secret" {
   value        = data.azurerm_key_vault_secret.hmc-servicebus-connection-string.value
   key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
 }
+
+resource "azurerm_servicebus_subscription_rule" "servicebus_subscription_rule_sscs" {
+  name            = "hmc-servicebus-subscription-rule=bba3"
+  subscription_id = azurerm_servicebus_subscription.servicebus_subscription.id
+  filter_type     = "CorrelationFilter"
+  
+  correlation_filter {
+    properties = {
+      hmctsServiceId = "BBA3"    
+    }
+  }
