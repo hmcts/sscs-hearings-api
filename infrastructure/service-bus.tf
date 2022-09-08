51c11c2e4a80b4a426eacdcc70687bf80ff88a96
Christopher Pateman <christopher.pateman@version1.com>
2022-09-08 15:18:40 +0100

    SSCS-10829 - update TF

--- infrastructure/service-bus.tf
+++ infrastructure/service-bus.tf
@@ -1,64 +1,49 @@
 #HMC to Hearings API
-module "servicebus-subscription" {
-  source                = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=master"
-  name                  = "hmc-to-sscs-subscription-${var.env}"
-  namespace_name        = "hmc-servicebus-${var.env}"
-  topic_name            = "hmc-to-cft-${var.env}"
-  resource_group_name   = "hmc-shared-${var.env}"
-}
-
-data "azurerm_key_vault" "hmc-key-vault" {
-  name                = "hmc-${var.env}"
+locals {
+  namespace_name      = "hmc-servicebus-${var.env}"
   resource_group_name = "hmc-shared-${var.env}"
+  topic_name          = "hmc-to-cft-${var.env}"
+  key_vault_secrets = [
+    "hmc-servicebus-connection-string",
+    "hmc-servicebus-shared-access-key"
+  ]
 }
 
-data "azurerm_key_vault_secret" "hmc-servicebus-connection-string" {
-  key_vault_id = data.azurerm_key_vault.hmc-key-vault.id
-  name         = "hmc-servicebus-connection-string"
+module "servicebus_subscription" {
+  source              = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=master"
+  name                = "hmc-to-sscs-subscription-${var.env}"
+  namespace_name      = local.namespace_name
+  topic_name          = local.topic_name
+  resource_group_name = local.resource_group_name
 }
 
-resource "azurerm_key_vault_secret" "hmc-servicebus-connection-string" {
-  name         = "hmc-servicebus-connection-string"
-  value        = data.azurerm_key_vault_secret.hmc-servicebus-connection-string.value
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
-}
-
-data "azurerm_key_vault_secret" "hmc-servicebus-shared-access-key" {
-  key_vault_id = data.azurerm_key_vault.hmc-key-vault.id
-  name         = "hmc-servicebus-shared-access-key"
-}
-
-resource "azurerm_key_vault_secret" "sscs-hmc-servicebus-hared-access-key" {
-  name         = "hmc-servicebus-shared-access-key"
-  value        = data.azurerm_key_vault_secret.hmc-servicebus-shared-access-key.value
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
-}
-  
-data "azurerm_servicebus_namespace" "hmc" {
-  name                = "hmc-servicebus-${var.env}" 
-  resource_group_name = "hmc-shared-${var.env}"
-}
-  
-resource "azurerm_servicebus_topic" "hmc-to-cft" {
-  name                = "hmc-to-cft"
-  namespace_id        = "data.azurerm_servicebus_namespace.servicebus_subscription_rule_sscs.id
-  enable_partitioning = true
-}
-
-resource "azurerm_servicebus_subscription" "hmc-to-cft" {
-  name               = "hmc-to-cft-${var.env}"
-  topic_id           = azurerum_servicebus_topic.servicebus_subscription_rule_sscs.id
-  max_delivery_count = 1
-}
-
-resource "azurerm_servicebus_subscription_rule" "servicebus_subscription_rule_sscs" {
-  name            = "hmc-servicebus-subscription-rule=bba3"
-  subscription_id = azurerm_servicebus_subscription.hmc_to_cft.id
+resource "azurerm_servicebus_subscription_rule" "rule_sscs" {
+  name            = "${local.namespace_name}-subscription-rule=bba3"
+  subscription_id = module.servicebus_subscription.id
   filter_type     = "CorrelationFilter"
-  
+
   correlation_filter {
-    properties     = {
-      hmctsServiceId = "BBA3"    
+    properties = {
+      hmctsServiceId = "BBA3"
     }
   }
 }
+
+data "azurerm_key_vault" "hmc" {
+  name                = "hmc-${var.env}"
+  resource_group_name = local.resource_group_name
+}
+
+data "azurerm_key_vault_secret" "this" {
+  for_each     = toset(local.key_vault_secrets)
+  key_vault_id = data.azurerm_key_vault.hmc.id
+  name         = each.key
+}
+
+resource "azurerm_key_vault_secret" "shared_access_key" {
+  for_each     = data.azurerm_key_vault_secret.this
+  name         = each.value.name
+  value        = each.value.value
+  key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
+}
+
