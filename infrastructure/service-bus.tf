726bd916a679b5bd86651010759cd8b55081418e
Christopher Pateman <christopher.pateman@version1.com>
2022-09-23 21:19:35 +0100

    Update TF version, Format TF, Remove non-required variable, Add descriptions to variables

--- infrastructure/service-bus.tf
+++ infrastructure/service-bus.tf
@@ -1,10 +1,10 @@
 #HMC to Hearings API
 module "servicebus-subscription" {
-  source                = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=master"
-  name                  = "hmc-to-sscs-subscription-${var.env}"
-  namespace_name        = "hmc-servicebus-${var.env}"
-  topic_name            = "hmc-to-cft-${var.env}"
-  resource_group_name   = "hmc-shared-${var.env}"
+  source              = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=master"
+  name                = "hmc-to-sscs-subscription-${var.env}"
+  namespace_name      = "hmc-servicebus-${var.env}"
+  topic_name          = "hmc-to-cft-${var.env}"
+  resource_group_name = "hmc-shared-${var.env}"
 }
 
 data "azurerm_key_vault" "hmc-key-vault" {
