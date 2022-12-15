ad1acb2a8c3847360b4f027698275ea57656549b
Nicholas Hill <nicholas.hill@HMCTS.net>
2022-12-15 16:25:13 +0000

    add line break between globals and constructor SSCS-11090

--- src/main/java/uk/gov/hmcts/reform/sscs/exception/ListingException.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/exception/ListingException.java
@@ -9,7 +9,9 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 @ResponseStatus(BAD_REQUEST)
 public class ListingException extends InvalidMappingException {
     private static final long serialVersionUID = -5687439455391806310L;
-
     public static final String SUMMARY = "Missing Listing Requirement";
-    public ListingException(String message) { super(message); }
+
+    public ListingException(String message) {
+        super(message);
+    }
 }
