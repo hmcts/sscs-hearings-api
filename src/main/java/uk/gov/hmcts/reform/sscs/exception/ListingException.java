5b3dd16c7a3859b5a16b8bef3fb9fe5dd4c4e3de
Nicholas Hill <nicholas.hill@HMCTS.net>
2022-12-21 12:11:13 +0000

    add suppression for netty SSCS-11090

--- src/main/java/uk/gov/hmcts/reform/sscs/exception/ListingException.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/exception/ListingException.java
@@ -1,11 +1,9 @@
 package uk.gov.hmcts.reform.sscs.exception;
 
-import lombok.Getter;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
-@Getter
 @ResponseStatus(BAD_REQUEST)
 public class ListingException extends InvalidMappingException {
     private static final long serialVersionUID = -5687439455391806310L;
