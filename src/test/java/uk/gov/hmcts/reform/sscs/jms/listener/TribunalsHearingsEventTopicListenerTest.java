6ca6d06f2d9908b88da3d23ce38ba137cc485b04
Nicholas Hill <nicholas.hill@HMCTS.net>
2022-12-22 15:00:42 +0000

    add test SSCS-11090

--- src/test/java/uk/gov/hmcts/reform/sscs/jms/listener/TribunalsHearingsEventTopicListenerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/jms/listener/TribunalsHearingsEventTopicListenerTest.java
@@ -9,9 +9,13 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.HearingRoute;
 import uk.gov.hmcts.reform.sscs.ccd.domain.HearingState;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 import uk.gov.hmcts.reform.sscs.exception.GetCaseException;
+import uk.gov.hmcts.reform.sscs.exception.ListingException;
 import uk.gov.hmcts.reform.sscs.exception.TribunalsEventProcessingException;
 import uk.gov.hmcts.reform.sscs.exception.UnhandleableHearingStateException;
 import uk.gov.hmcts.reform.sscs.exception.UpdateCaseException;
@@ -21,11 +25,12 @@ import uk.gov.hmcts.reform.sscs.service.HearingsService;
 
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TribunalsHearingsEventTopicListenerTest {
@@ -39,6 +44,8 @@ class TribunalsHearingsEventTopicListenerTest {
     @Mock
     private CcdCaseService ccdCaseService;
 
+    private static final String CASE_ID = "1001";
+
     @Test
     @DisplayName("When a valid request comes in make sure processHearingRequest is hit")
     void whenAValidRequestComesIn_makeSureProcessHearingRequestIsHit() throws Exception {
@@ -47,7 +54,7 @@ class TribunalsHearingsEventTopicListenerTest {
 
         tribunalsHearingsEventQueueListener.handleIncomingMessage(hearingRequest);
 
-        verify(hearingsService, times(1)).processHearingRequest((eq(hearingRequest)));
+        verify(hearingsService, times(1)).processHearingRequest((hearingRequest));
     }
 
     @ParameterizedTest
@@ -57,7 +64,7 @@ class TribunalsHearingsEventTopicListenerTest {
 
         HearingRequest hearingRequest = new HearingRequest();
 
-        doThrow(throwable).when(hearingsService).processHearingRequest(eq(hearingRequest));
+        doThrow(throwable).when(hearingsService).processHearingRequest(hearingRequest);
 
         assertThrows(TribunalsEventProcessingException.class, () -> tribunalsHearingsEventQueueListener.handleIncomingMessage(hearingRequest));
     }
@@ -77,9 +84,29 @@ class TribunalsHearingsEventTopicListenerTest {
     }
 
     private HearingRequest createHearingRequest() {
-        return HearingRequest.builder("1001")
+        return HearingRequest.builder(CASE_ID)
             .hearingRoute(HearingRoute.LIST_ASSIST)
             .hearingState(HearingState.CREATE_HEARING)
             .build();
+    }
+
+    @Test
+    @DisplayName("When a listing exception is thrown, catch the error and update the state")
+    void whenListingExceptionThrown_UpdateCaseDataState() throws Exception {
+        SscsCaseData caseData = SscsCaseData.builder().build();
+        SscsCaseDetails caseDetails = SscsCaseDetails.builder().data(caseData).build();
+        HearingRequest hearingRequest = createHearingRequest();
+
+        doThrow(ListingException.class)
+            .when(hearingsService)
+            .processHearingRequest(hearingRequest);
+
+        when(ccdCaseService.getCaseDetails(CASE_ID)).thenReturn(caseDetails);
+        when(ccdCaseService.updateCaseData(caseData,
+                                           EventType.LISTING_ERROR,
+                                           null,
+                                           null)).thenReturn(caseDetails);
+
+        assertDoesNotThrow(() -> tribunalsHearingsEventQueueListener.handleIncomingMessage(hearingRequest));
     }
 }
