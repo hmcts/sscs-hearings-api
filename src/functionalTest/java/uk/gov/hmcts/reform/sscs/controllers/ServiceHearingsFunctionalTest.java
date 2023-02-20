ae079a0dcdc2c381c2f78805af3e7f41bb85b355
DGordonV1 <david.gordon@hmcts.net>
2023-02-20 08:48:13 +0000

    SSCS-11357 - Remove unused import

--- src/functionalTest/java/uk/gov/hmcts/reform/sscs/controllers/ServiceHearingsFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sscs/controllers/ServiceHearingsFunctionalTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.sscs.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
