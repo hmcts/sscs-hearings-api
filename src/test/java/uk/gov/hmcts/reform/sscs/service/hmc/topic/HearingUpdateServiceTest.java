08bd1e3ecf6f3d282e244bb33989a6b92d7d3a0e
DGordonV1 <david.gordon@hmcts.net>
2024-03-04 12:35:19 +0000

    Fix imports

--- src/test/java/uk/gov/hmcts/reform/sscs/service/hmc/topic/HearingUpdateServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/service/hmc/topic/HearingUpdateServiceTest.java
@@ -31,8 +31,8 @@ import uk.gov.hmcts.reform.sscs.service.VenueService;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
