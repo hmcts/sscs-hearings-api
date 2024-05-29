30f535089c5ef60c07cd448f6a284208cdd008a8
andrewmcmahon <andrew.mcmahon@solirius.com>
2024-05-29 15:33:02 +0100

    Revert ProcessHmcMessageService.java to master

--- src/main/java/uk/gov/hmcts/reform/sscs/jms/listener/HmcHearingsEventTopicListener.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/jms/listener/HmcHearingsEventTopicListener.java
@@ -71,7 +71,7 @@ public class HmcHearingsEventTopicListener {
                     hmcMessage.getHearingUpdate().getHmcStatus(), caseId, hearingId);
 
                 if (processEventMessageV2Enabled) {
-                    processHmcMessageService.processEventMessageV2(hmcMessage);
+                    processHmcMessageService.processEventMessage(hmcMessage);
                 } else {
                     processHmcMessageService.processEventMessage(hmcMessage);
                 }
