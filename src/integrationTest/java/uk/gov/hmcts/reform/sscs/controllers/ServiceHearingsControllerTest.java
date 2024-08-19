58b640fe57ae373cb74fa33a337d8bad516c5366
taleb <tech@benouaer.com>
2024-08-19 18:17:06 +0100

    SSCSCI-976: limit mocks to only external api calls

--- src/integrationTest/java/uk/gov/hmcts/reform/sscs/controllers/ServiceHearingsControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/sscs/controllers/ServiceHearingsControllerTest.java
@@ -13,14 +13,26 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import uk.gov.hmcts.reform.sscs.exception.GetCaseException;
+import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.ccd.client.model.SearchResult;
+import uk.gov.hmcts.reform.sscs.ccd.client.CcdClient;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Appeal;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Appellant;
+import uk.gov.hmcts.reform.sscs.ccd.domain.CaseLink;
+import uk.gov.hmcts.reform.sscs.ccd.domain.HearingOptions;
+import uk.gov.hmcts.reform.sscs.ccd.domain.HearingSubtype;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Name;
+import uk.gov.hmcts.reform.sscs.idam.IdamService;
+import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
 import uk.gov.hmcts.reform.sscs.model.service.ServiceHearingRequest;
-import uk.gov.hmcts.reform.sscs.model.service.hearingvalues.ServiceHearingValues;
-import uk.gov.hmcts.reform.sscs.model.service.linkedcases.ServiceLinkedCases;
-import uk.gov.hmcts.reform.sscs.service.ServiceHearingsService;
 
 import java.util.List;
+import java.util.Map;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -46,14 +58,19 @@ class ServiceHearingsControllerTest {
     private static final ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
 
     public static final String PROCESSING_VENUE = "Liverpool";
-
-    public static final String BASE_LOCATION = "12345";
+    public static final String BENEFIT_CODE = "1";
+    public static final String ISSUE_CODE = "UM";
 
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
-    public ServiceHearingsService serviceHearingsService;
+    private CcdClient ccdClient;
+    @MockBean
+    private IdamService idamService;
+    @MockBean
+    private CoreCaseDataApi coreCaseDataApi;
+
 
 
     @DisplayName("When Authorization and Case ID valid "
@@ -64,9 +81,20 @@ class ServiceHearingsControllerTest {
                 .caseId(String.valueOf(CASE_ID))
                 .build();
 
-        given(serviceHearingsService.getServiceHearingValues(request))
-            .willReturn(ServiceHearingValues.builder()
-                .build());
+        given(ccdClient.readForCaseworker(any(), eq(CASE_ID)))
+            .willReturn(CaseDetails.builder().data(
+                Map.of("appeal",
+                       Appeal.builder()
+                           .appellant(Appellant.builder()
+                                          .name(Name.builder().firstName("John").lastName("Smith").build())
+                                          .build())
+                           .hearingSubtype(HearingSubtype.builder().wantsHearingTypeFaceToFace("yes").build())
+                           .hearingOptions(HearingOptions.builder().wantsToAttend("yes").build()).build(),
+                       "benefitCode", BENEFIT_CODE,
+                       "issueCode", ISSUE_CODE,
+                       "processingVenue", PROCESSING_VENUE
+                )
+            ).build());
 
         mockMvc.perform(post(SERVICE_HEARING_VALUES_URL)
                         .contentType(APPLICATION_JSON)
@@ -82,14 +110,43 @@ class ServiceHearingsControllerTest {
                 .caseId(String.valueOf(MISSING_CASE_ID))
                 .build();
 
-        given(serviceHearingsService.getServiceHearingValues(request))
-            .willThrow(GetCaseException.class);
+        given(ccdClient.readForCaseworker(any(), eq(MISSING_CASE_ID)))
+            .willReturn(null);
 
         mockMvc.perform(post(SERVICE_HEARING_VALUES_URL)
                         .contentType(APPLICATION_JSON)
                         .content(asJsonString(request)))
                 .andDo(print())
                 .andExpect(status().isNotFound());
+    }
+
+    @DisplayName("When Request and Authorisation are valid should return all mapped hearing service values")
+    @Test
+    void shouldReturnAllMappedServiceHearingValues() throws Exception {
+        ServiceHearingRequest request = ServiceHearingRequest.builder()
+                .caseId(String.valueOf(CASE_ID))
+                .build();
+
+        given(ccdClient.readForCaseworker(any(), eq(CASE_ID)))
+            .willReturn(CaseDetails.builder().data(
+                Map.of("appeal",
+                       Appeal.builder()
+                           .appellant(Appellant.builder()
+                                          .name(Name.builder().firstName("John").lastName("Smith").build())
+                                          .build())
+                           .hearingSubtype(HearingSubtype.builder().wantsHearingTypeFaceToFace("yes").build())
+                           .hearingOptions(HearingOptions.builder().wantsToAttend("yes").build()).build(),
+                       "benefitCode", BENEFIT_CODE,
+                       "issueCode", ISSUE_CODE,
+                       "processingVenue", PROCESSING_VENUE
+                )
+            ).build());
+
+        mockMvc.perform(post(SERVICE_HEARING_VALUES_URL)
+                        .contentType(APPLICATION_JSON)
+                        .content(asJsonString(request)))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @DisplayName("When Authorization and Case ID valid should return the case name with a with 200 response code")
@@ -100,10 +157,13 @@ class ServiceHearingsControllerTest {
             .hearingId(String.valueOf(HEARING_ID))
             .build();
 
-        given(serviceHearingsService.getServiceLinkedCases(request))
-            .willReturn(List.of(ServiceLinkedCases.builder()
-                .build()));
-
+        given(idamService.getIdamTokens()).willReturn(IdamTokens.builder().build());
+        given(coreCaseDataApi.searchCases(
+            any(), any(), any(), contains(String.valueOf(CASE_ID))))
+            .willReturn(SearchResult.builder().cases(
+                List.of(CaseDetails.builder()
+                            .data(Map.of("linkedCase", List.of(CaseLink.builder().build()))).build()
+                )).build());
 
         mockMvc.perform(post(SERVICE_LINKED_CASES_URL)
                         .contentType(APPLICATION_JSON)
@@ -119,8 +179,10 @@ class ServiceHearingsControllerTest {
                 .hearingId(String.valueOf(HEARING_ID))
                 .build();
 
-        given(serviceHearingsService.getServiceLinkedCases(request))
-            .willThrow(GetCaseException.class);
+        given(idamService.getIdamTokens()).willReturn(IdamTokens.builder().build());
+        given(coreCaseDataApi.searchCases(
+            any(), any(), any(), contains(String.valueOf(MISSING_CASE_ID))))
+            .willReturn(SearchResult.builder().build());
 
         mockMvc.perform(post(SERVICE_LINKED_CASES_URL)
                         .contentType(APPLICATION_JSON)
